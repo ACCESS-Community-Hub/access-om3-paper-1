ba48c375a7a20a68b28f1330834d1b5849c1cea1
Christopher Bull <5499680+chrisb13@users.noreply.github.com>
2025-08-29 17:03:56 +1000

    Update mkfigs.sh
    no dice for the internet on a compute node so updating.

--- notebooks/mkfigs.sh
+++ notebooks/mkfigs.sh
@@ -10,25 +10,23 @@
 #PBS -o /g/data/tm70/as2285/logs
 #PBS -e /g/data/tm70/as2285/logs
 
+# bash script that runs all the notebooks
+set -x
 module use /g/data/xp65/public/modules
 module load conda/analysis3
 module load conda/analysis3-25.07
-module load git
 module list
 
-# bash script that runs all the notebooks
-set -x
-
 # SET THESE START
-WFOLDER=/g/data/tm70/cyb561/
+#WFOLDER is where you have cloned: `git clone git@github.com:ACCESS-Community-Hub/access-om3-paper-1.git `
+WFOLDER=/g/data/tm70/cyb561/access-om3-paper-1/
 ESMDIR=/g/data/ol01/access-om3-output/access-om3-025/MC_25km_jra_ryf-1.0-beta/experiment_datastore.json
 # SET THESE END
 
-PFOL=${WFOLDER}+access-om3-paper-1/notebooks/plots/
+PFOL=${WFOLDER}+notebooks/plots/
 
 cd ${WFOLDER}
-git clone git@github.com:ACCESS-Community-Hub/access-om3-paper-1.git 
-cd access-om3-paper-1/notebooks
+cd notebooks
 mkdir -p ${PFOL}
 
 python run_nb.py notebook_template.ipynb ${ESMDIR} --plotfolder ${PFOL}
