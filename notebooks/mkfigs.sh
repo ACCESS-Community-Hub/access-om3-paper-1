389ad17c1946e9bed53a53be75df01edd492fa04
Christopher Bull <5499680+chrisb13@users.noreply.github.com>
2025-09-04 12:21:00 +1000

    Modifying notebooks to use mkfigs.sh v2 (papermill) (#32)
    Overview: updates the repo to be able to use papermill to run most of the current notebooks. The following notebooks are supported: notebook_template DrakePassageTransport GlobalTimeseries Overturning_in_ACCESS_OM3 find_and_load_OM3_25km_ryf_1.0-beta SSS SST StraitTransports. Detailed (squashed) commit history:
    
    * first test at running useful scripts on pbs
    
    * Update mkfigs.sh
    
    no dice for the internet on a compute node so updating.
    
    * Update mkfigs.sh
    
    just some pbs log changes
    
    * Update mkfigs.sh
    
    one typos and fixes
    
    * Update mkfigs.sh
    
    Forgot xp65!
    
    * Update mkfigs.sh
    
    too much outtput!
    
    * adding overturning
    
    * can now render figures on their own
    
    * first commit using papermill, works!
    
    * updated all the current notebooks to use the new papermill workflow

--- notebooks/mkfigs.sh
+++ notebooks/mkfigs.sh
@@ -1,7 +1,70 @@
 #!/bin/bash
-# bash script that runs all the notebooks
-set -x
-ESMDIR=/g/data/ol01/access-om3-output/access-om3-025/MC_25km_jra_ryf-1.0-beta/experiment_datastore.json
-PFOL=/home/561/cyb561/repos/access-om3-paper-1/notebooks/plots/
+#PBS -l storage=gdata/tm70+gdata/ik11+gdata/ol01+gdata/xp65
+#PBS -M chris.bull@anu.edu.au
+#PBS -m ae
+#PBS -q normal
+#PBS -W umask=0022
+#PBS -l ncpus=8
+#PBS -l mem=24gb
+#PBS -l walltime=2:00:00
+#PBS -o /g/data/tm70/cyb561/logs
+#PBS -e /g/data/tm70/cyb561/logs
 
-python run_nb.py notebook_template.ipynb ${ESMDIR} --plotfolder ${PFOL}
+# bash script that runs all the notebooks
+#set -x
+module purge
+module use /g/data/xp65/public/modules
+module load conda/analysis3-25.07
+module list
+
+#enable venv with papermill
+source /g/data/tm70/cyb561/access-om3-paper-1/venv/bin/activate
+
+## workflow
+#1. `cd /g/data/tm70/cyb561;git clone git@github.com:ACCESS-Community-Hub/access-om3-paper-1.git`
+#1. Edit this file and `chmod u+x mkfigs.sh`
+#1. add path to WFOLDER
+#1. set path to ESMDIR (ESM-datastore for experiment)
+#1. ensure the experiment folder is availble in storage header above
+#1. `qsub mkfigs.sh`
+
+## Optional
+#1. change email and log settings in above header
+#1. this script can also be run from an ARE session
+
+
+# SET THESE START
+WFOLDER=/g/data/tm70/cyb561/access-om3-paper-1/
+ESMDIR=/g/data/ol01/access-om3-output/access-om3-025/MC_25km_jra_ryf-1.0-beta/experiment_datastore.json
+
+#DS run from June 2025
+#ESMDIR=/scratch/tm70/ds0092/access-om3/archive/om3_MC_25km_jra_ryf+wombatlite/intake_esm_ds.json
+
+# SET THESE END
+
+#best not mess with the path here...
+OFOL=${WFOLDER}notebooks/mkfigs_output/
+
+cd ${WFOLDER}
+cd notebooks
+mkdir -p ${OFOL}
+
+echo ""
+echo ""
+echo "We are running ALL the notebooks."
+echo "We are using ESMDIR: "${ESMDIR}
+echo "We are using working folder (WFOLDER): "${WFOLDER}
+echo "Output will be in: "${OFOL}
+echo ""
+echo ""
+
+#make the figures
+array=( notebook_template DrakePassageTransport GlobalTimeseries Overturning_in_ACCESS_OM3 find_and_load_OM3_25km_ryf_1.0-beta SSS SST StraitTransports )
+for FNAME in "${array[@]}"
+do
+    echo "Running notebook: "${FNAME}".ipynb"
+    python3 run_nb.py ${FNAME}.ipynb; papermill ${FNAME}.ipynb ${OFOL}${FNAME}_rendered.ipynb -p esm_file ${ESMDIR} -p plotfolder ${OFOL} ; jupyter nbconvert --to markdown ${OFOL}${FNAME}_rendered.ipynb
+done
+
+#FYI for why run_nb was needed; this didn't work for me...
+#papermill notebook_template.ipynb notebook_template_rendered.ipynb -k analysis3-25.07 -p esm_file ${ESMDIR} -p plotfolder ${OFOL} 
--- notebooks/run_nb.py
+++ notebooks/run_nb.py
@@ -1,19 +1,8 @@
-#inspired by
-#https://stackoverflow.com/questions/37534440/passing-command-line-arguments-to-argv-in-jupyter-ipython-notebook
 import sys,os
 import json
-CONFIG_FILENAME = 'config_ipynb_tmp'
+import glob
 
 def main(argv):
-    try:
-        os.remove(CONFIG_FILENAME)
-    except OSError:
-        pass
-
-    with open(CONFIG_FILENAME,'w') as f:
-        f.write(' '.join(argv))
-
-    #import pdb;pdb.set_trace()
     #cb: 29/8/2025
     #https://access-nri.zulipchat.com/#narrow/channel/470323-med-team/topic/No.20such.20kernel.20named.20conda-env-analysis3-25.2E07-py.3F/with/536711167
     #yeah so the problem was when one opens a notebook in ARE it assigns a kernal that can't used on the command line, thanks Charles Turner!!
@@ -27,15 +16,13 @@ def main(argv):
     d['metadata']['kernelspec']['name']='python3'
     #print(d['metadata']['kernelspec'])
 
-    newf=argv[1][:-6]+'_new.ipynb'
+    newf=argv[1][:-6]+'_tmp.ipynb'
     with open(newf, "w") as jsonFile:
         json.dump(d, jsonFile)
+    #import pdb;pdb.set_trace()
 
-    os.system('jupyter nbconvert --execute {:s} --to html'.format(newf))
-    os.remove(newf)
-    os.rename(newf[:-6]+'.html',newf[:-10]+'.html')
-    print('File created: '+newf[:-10]+'.html')
-
+    os.rename(newf,argv[1])
+    
     return None
 
 if __name__ == '__main__':
