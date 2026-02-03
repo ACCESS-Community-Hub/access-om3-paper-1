282422c816f9ff0e0498fb17f219d00817716a73
Christopher Bull <5499680+chrisb13@users.noreply.github.com>
2026-02-03 12:32:32 +1100

    adding the boilerplate to all the notebooks in mkfigs and small updates to mkfigs_configdoc.py to allow adding multiple figures

--- notebooks/mkfigs_configdoc.py
+++ notebooks/mkfigs_configdoc.py
@@ -2,14 +2,15 @@ import os
 #little function to create a figure file for om3 configs
 #cb
 def mkmd(title,caption,experiment,plot_fname,mdfol):
-    print('')
-    print(title)
-    print(caption)
-    print(experiment)
-    print(plot_fname)
-    print('')
+    #print('')
+    #print(title)
+    #print(caption)
+    #print(experiment)
+    #print(plot_fname)
+    #print('')
 
     mdpath=mdfol+experiment+'.md'
+    print('Adding a figure to markdown doc: '+mdpath)
 
     lines_to_append = [
         "# "+experiment+"\n",
@@ -17,7 +18,7 @@ def mkmd(title,caption,experiment,plot_fname,mdfol):
         "# This page shows evaluation figures from ACCESS-OM3 experiment "+ experiment+ " for discussion and see plotting scripts have a look at [this repository](https://github.com/acCESS-Community-Hub/access-om3-paper-1/) and related [issues](https://github.com/ACCESS-Community-Hub/access-om3-paper-1/issues).\n",
         " \n",
         "## "+title+"\n",
-        "<!-- The next line will hopefully be a rendered figure. --> \n",
+        " \n",
         '!['+caption+'](mkmd/'+plot_fname+') \n',
         "  Caption: "+caption+"\n",
         "  \n"
@@ -25,10 +26,12 @@ def mkmd(title,caption,experiment,plot_fname,mdfol):
 
     #check if file exists
     if os.path.exists(mdpath):
-        lines_to_append=lines_to_append[3:]
+        lines_to_append=lines_to_append[4:]
 
-    #should add a check to see if title already exists :q
-
+    #should add a check to see if title already exists 
+    if string_exists_in_file(mdpath, title):
+        print("This notebook has already added to the figure file, so this will add an additional figure.")
+        lines_to_append=lines_to_append[5:]
 
     try:
         with open(mdpath, 'a') as file:
@@ -37,4 +40,16 @@ def mkmd(title,caption,experiment,plot_fname,mdfol):
     except Exception as e:
         print(f"Error: {e}")
     return
+
+def string_exists_in_file(filename, search_string):
+    """Checks if a string exists in a file (case-sensitive)."""
+    try:
+        with open(filename, 'r') as myfile:
+            if search_string in myfile.read():
+                return True
+            else:
+                return False
+    except FileNotFoundError:
+        print(f"Error: The file '{filename}' was not found.")
+        return False
 
